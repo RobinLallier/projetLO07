accf7e8b11194411f45cde048a9dc1829b8a2662
Bertille Lagorce <bertillelagorce@gmail.com>
2018-05-30 14:50:41 +0200

    inscription nounou

--- NounousLO07/html/inscriptionNounou.php
+++ NounousLO07/html/inscriptionNounou.php
@@ -17,50 +17,87 @@
     <h1 class="titreaccueil">maNounou.com</h1>
     <h2 class="lead">Le meilleur site de recherche de nounous</h2>
 </header>
- <form method="GET" action="board_nounou.html">
-     <p>
-     <fieldset>
-         <legend>Veuillez rentrez les informations suivantes</legend>
-         <br>
-         Nom :
-         &nbsp;
-         <input type="text" name="nom" value=""/>
-         <br><br>
-         Prénom :
-         &nbsp;
-         <input type="text" name="prenom" value=""/>
-         <br><br>
-         Ville :
-         &nbsp;
-         <input type="text" name="ville" value="Entrez le code postal"/>
-         <br><br>
-         Adresse e-mail :
-         &nbsp;
-         <input type="text" name="mail" value=""/>
-         <br><br>
-         Numéro de téléphone :
-         &nbsp;
-         <input type="text" name="ville" value=""/>
-         <br><br>
-         Age :
-         &nbsp;
-         <input type="number" name="age" value="0">
-         <br><br>
-         Veuillez charger votre photo :
-         &nbsp;
-         <input type="file" name="photo">
-         <br><br>
-         Années d'expérience :
-         &nbsp;
-         <input type="number" name="experience" value="0">
-         <br><br>
-         Description:
-         &nbsp;
-         <textarea name="description">Expérience en tant que nounou</textarea>
-         <br><br>
-         <input type="submit" name="envoi" value="S'inscrire">
+<nav class="nav justify-content-end">
+    <a href="../php/config.php" class="nav-item ">Se Connecter</a>
 
+</nav>
+<div class="row">
+    <div class="jumbotron col-10 offset-md-1">
+ <form method="post" action="../php/inscription.php">
+     <fieldset>
+         <legend>Veuillez rentrez les informations suivantes :</legend>
+         <div class="form-group">
+             <label for="login">Login</label>
+             <input type="text" class="form-control" id="login" placeholder="Choisissez un login">
+
+         </div>
+         <div class="form-group">
+             <label for="password">Mot de Passe</label>
+             <input type="password" class="form-control" id="password" placeholder="Votre mot de passe">
+             <small id="emailHelp" class="form-text text-muted">Votre mot de passe restera confidentiel.</small>
+         </div>
+        <br>
+        <br>
+     <div class="form-group">
+         <label for="nom">Nom</label>
+         <input type="text" class="form-control" id="nom" placeholder="Votre nom">
+
+     </div>
+
+     <div class="form-group">
+         <label for="prenom">Prénom</label>
+         <input type="text" class="form-control" id="prenom" placeholder="Votre prénom">
+
+         <div class="form-group">
+             <label for="ville">Ville</label>
+             <input type="text" class="form-control" id="ville" placeholder="Votre code postal">
+
+         </div>
+
+         <div class="form-group">
+             <label for="email">Adresse eMail</label>
+             <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Votre email">
+
+         </div>
+
+         <div class="form-group">
+             <label for="numero">Numéro de téléphone</label>
+             <input type="text" class="form-control" id="numero" placeholder="Votre numéro de portable">
+
+         </div>
+
+         <div class="form-group">
+             <label for="age">Age</label>
+             <input type="number" class="form-control" id="age" placeholder="">
+
+         </div>
+
+         <div class="form-group">
+             <label for="photo">Votre photo</label>
+             <input type="file" class="form-control" id="photo" placeholder="">
+
+         </div>
+
+         <div class="form-group">
+             <label for="experience">Années d'expérience</label>
+             <input type="number" class="form-control" id="experience" placeholder="">
+
+         </div>
+
+         <div class="form-group">
+             <label for="description">Description</label>
+             <input type="textarea" class="form-control" id="description" placeholder="">
+         </div>
+
+         <div class="form-check">
+             <input type="checkbox" class="form-check-input" id="exampleCheck1">
+             <label class="form-check-label" for="exampleCheck1">J'accepte les conditions générales d'utilisation</label>
+         </div>
+ <br>
+         <button type="submit" class="btn btn-primary">M'inscrire</button>
      </fieldset>
+
  </form>
+    </div>
 </body>
 </html>
