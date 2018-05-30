7867dc8dc7639f924e9a8ab6f3c3bfd435f7a435
Robin Lallier <robin.lallier29@gmail.com>
2018-05-30 14:47:35 +0200

    login

---
+++ NounousLO07/php/connexion.php
@@ -0,0 +1,33 @@
+<ul>
+<?php
+include 'config.php';
+
+if(isset($_POST["login"]) && ($_POST["login"] != "")) printf("<li><b>login</b> = %s</li>", $_POST["login"]);
+else(printf("<li><h2>PAS DE LOGIN FOURNI</h2></li>"));
+if(isset($_POST["password"]) && ($_POST["password"] != "")) printf("<li><b>mot de passe</b> = %s</li>", $_POST["password"]);
+else(printf("<li><h2>PAS DE mot de passe FOURNI</h2></li>"));
+?>
+
+<?php
+//Vérification de la validité des informations entrées
+if( isset($_POST["login"]) && isset($_POST["password"])) {
+
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+    //Hachage du mot de passe
+    //$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    if( mysqli_query($bdd, "SELECT * FROM UTILISATEURS WHERE mdp=$password AND login=$login; ")){
+        if(mysqli_query($bdd, "SELECT admin FROM UTILISATEURS WHERE mdp=$password AND login=$login; ") === "1") {
+            echo("<h1> Cette personne est un admin</h1>");
+        }
+        else{
+            echo("<h1>Cette personne n'est pas un admin</h1>");
+        }
+    }
+
+
+
+
+}
+?>
--- NounousLO07/php/login.php
+++ NounousLO07/php/login.php
@@ -1,35 +1,50 @@
 <?php
-include("config.php");
-session_start();
+ include 'config.php';
+?>
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // username and password sent from form
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    $myusername = mysqli_real_escape_string($db,$_POST['username']);
-    $mypassword = mysqli_real_escape_string($db,$_POST['password']);
+    <title>MaNounou.com</title>
 
-    $sql = "SELECT categorie FROM nom_table_utilisateur WHERE username = '$myusername' and passcode = '$mypassword'";
-    $result = mysqli_query($db,$sql);
-    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-    $active = $row['active'];
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body class="container-fluid">
+<header class="text-center container-fluid">
+    <a href="../index.html" >
+        <img src="../img/rattle.png" class="float-left logo" alt="hochet">
+    </a>
+    <h1>maNounou.com</h1>
+    <h2 class="lead">Le meilleur site de recherche de nounous</h2>
+</header>
+<nav class="nav justify-content-end">
+    <a href="../php/config.php" class="nav-item ">Se Connecter</a>
 
-    $count = mysqli_num_rows($result);
+</nav>
+<div class="row">
+    <div class="jumbotron col-10 offset-md-1">
+        <form method="post" action="connexion.php">
+            <div class="form-group">
+                <label for="login">Login</label>
+                <input type="text" class="form-control" id="login" placeholder="Votre login">
 
-    // If result matched $myusername and $mypassword, table row must be 1 row
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de Passe</label>
+                <input type="password" class="form-control" id="password" placeholder="Votre mot de passe">
+                <small id="emailHelp" class="form-text text-muted">Votre mot de passe restera confidentiel.</small>
+            </div>
 
-    if($count == 1) {
-        session_register("myusername");
-        $_SESSION['login_user'] = $myusername;
-        if($result === "nounou"){
-
-            header("location : tableaunounou.php");
-        }
-        else{
-            header("location: tableauparent.php");
-        }
+            <button type="submit" class="btn btn-primary">Me Connecter</button>
+        </form>
+    </div>
+</div>
 
 
-    }else {
-        $error = "Your Login Name or Password is invalid";
-    }
-}
+
+</body>
+</html>
